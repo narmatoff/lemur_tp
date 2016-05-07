a12217eddc24184dae03ad37b644bc828a4e2297
narmatoff <narmatoff@ya.ru>
2016-05-07 19:21:04 +0300

    logos slider started

--- src/js_src/custom_plgns.js
+++ src/js_src/custom_plgns.js
@@ -1,3 +1,4 @@
 $(document).ready(function() {
-    console.log('custom');
+    console.log('custom_plgns.js');
+    
 });
--- src/js_src/main.js
+++ src/js_src/main.js
@@ -1,13 +1,22 @@
 $(document).ready(function() {
-    console.log('main');
+    console.log('main.js');
 
-    var swiper = new Swiper('.swiper-container', {
+    var swiper = new Swiper('.swiper-tarifs', {
         //    pagination: '.swiper-pagination',
-        nextButton: '.left__heading__buttons__wr__next',
-        prevButton: '.left__heading__buttons__wr__prev',
+        nextButton: '.tarifs_next',
+        prevButton: '.tarifs_prev',
         slidesPerView: 4,
         paginationClickable: true,
         spaceBetween: 32
+    });
+
+    var swiper = new Swiper('.swiper-logos', {
+        //    pagination: '.swiper-pagination',
+        // nextButton: '.left__heading__buttons__wr__next',
+        // prevButton: '.left__heading__buttons__wr__prev',
+        slidesPerView: 7,
+        paginationClickable: true,
+        spaceBetween: 37
     });
 
 });
